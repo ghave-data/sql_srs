c0a566f7db824c6eb67ae6bc12d06a4752ea74ba
Guillaume Havé <guillaume.have@orange.fr>
2024-10-08 21:34:07 +0200

    update error managment app & workflow pull request

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+# pylint: disable=missing-module-docstring
 import io
 
-csv = '''
+import duckdb
+import pandas as pd
+import streamlit as st
+
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
+"""
 
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
-food_item,price
+CSV2 = """
+food_item,food_price
 cookie,2.5
 chocolatine,2
 muffin,3
-'''
+"""
 
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STRING = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution = duckdb.sql(answer)
+solution_df = duckdb.sql(ANSWER_STRING).df()
 
 with st.sidebar:
     option = st.selectbox(
         "How would you like to review?",
-        ("Joins","GroupBy", "Windows Functions"),
+        ("Joins", "GroupBy", "Windows Functions"),
         index=None,
         placeholder="Select contact method...",
     )
@@ -41,19 +43,25 @@ with st.sidebar:
 st.header("Enter your code:")
 query = st.text_area(label="your SQL code here", key="user_input")
 if query:
-    result = duckdb.sql(query)
+    result = duckdb.sql(query).df()
     st.dataframe(result)
 
-    if len(result.columns) != len(solution.columns):
+    if len(result.columns) != len(solution_df.columns):
         st.write("Some columns are missing")
 
-    n_lines_difference = result.shape[0] - solution.shape[0]
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
     if n_lines_difference != 0:
         st.write(
-            f"resultat has a {n_lines_difference} lines difference with the solution"
+            f"result has a {n_lines_difference} lines difference with the solution_df"
         )
 
-tab1, tab2 = st.tabs(["Tables","Solutions"])
+tab1, tab2 = st.tabs(["Tables", "Solutions"])
 
 with tab1:
     st.write("table: beverages")
@@ -61,7 +69,7 @@ with tab1:
     st.write("table: food_items")
     st.dataframe(food_items)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab2:
-    st.write(answer)
+    st.write(ANSWER_STRING)
