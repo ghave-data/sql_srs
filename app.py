f3fd673ee597e34df73cfbbecb3c516be10d5ecb
Guillaume Havé <guillaume.have@orange.fr>
2024-10-09 19:51:49 +0200

    update app

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import ast
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-#solution_df = duckdb.sql(ANSWER_STRING).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "How would you like to review?",
@@ -20,13 +18,19 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Enter your code:")
 query = st.text_area(label="your SQL code here", key="user_input")
 
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-    """
+
     if len(result.columns) != len(solution_df.columns):
         st.write("Some columns are missing")
 
@@ -41,18 +45,17 @@ if query:
         st.write(
             f"result has a {n_lines_difference} lines difference with the solution_df"
         )
-"""
+
 tab1, tab2 = st.tabs(["Tables", "Solutions"])
 
 with tab1:
     exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
     for table in exercise_tables:
-        st.write(f"table: {table}")
+        st.write(f"Table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()
         st.dataframe(df_table)
+    st.write("Expected result:")
+    st.dataframe(solution_df)
 
 with tab2:
-    ANSWER_STRING = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{ANSWER_STRING}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
